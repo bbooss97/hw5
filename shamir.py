27d927941ee08ffb25e9dc35b3e5b843dea5c522
bbooss97 <bbooss97@gmail.com>
2021-12-06 23:48:26 +0100

    alnmost

--- shamir.py
+++ shamir.py
@@ -4,85 +4,68 @@ from decimal import *
 from matplotlib import pyplot as plt
 from scipy.interpolate import lagrange
 from numpy.polynomial.polynomial import Polynomial
-getcontext().prec=10**5
-maxRandomCoefficients =10*6
+getcontext().prec = 10**5
+maxRandomCoefficients = 10*5
 
 
 def drawFunction(coefficients):
-    size=10**10
-    x=np.linspace(-size,size)
-    y=[]
-    for i in range(len(x)):
-        yy=0
-        for j in range(len(coefficients)):
-            yy+=(x[i]**j)*coefficients[j]
-        y.append(yy)
-    plt.plot(x,y)
+    size = 1000000
+    x = np.linspace(-size, size,1000000)
+    y=np.polyval(coefficients[::-1],x)
+    plt.plot(x, y)
     plt.grid()
+    plt.axhline(y=0, color='r')
+    plt.axvline(x=0, color='r')
     plt.show()
 
-def createShards(k,n,s,draw):
-    coefficients=[r.randint(0,maxRandomCoefficients) for i in range(k)]
-    coefficients[0]=s
-    shards=[]
-    
-    for i in range(n):
-        y=0
-        x=r.randint(0,maxRandomCoefficients)
-        for j in range(k):
-            y+=(x**j)*coefficients[j]
-        shards.append((x,y))
-    if draw==True:
-        drawFunction(coefficients)
-    print(coefficients)    
 
+def createShards(k, n, s, draw):
+    g=0
+    coefficients =[r.randint(0, maxRandomCoefficients) for i in range(k)]
+    coefficients[0] = s
+    print(coefficients)
+    shards = []
+    for i in range(n):
+        y = 0
+        l=[i[0] for i in shards]
+        x=0
+        while(True):
+            x = r.randint(0, maxRandomCoefficients)
+            if x not in l:
+                break
+        for j in range(k):
+            y += (x**j)*coefficients[j]
+        shards.append((x, y))
+    if draw == True:
+        drawFunction(coefficients)
     return shards
 
 
-
-def findSecrett(shards,k):
-    if len(shards)<k:
+def findSecrett(shards, k):
+    if len(shards) < k:
         print("you need more shards")
         return -1
-    secret=Decimal(0)
+    secret = Decimal(0)
     for i in range(k):
-        li=Decimal(1)
-        xi=shards[i][0]
-        yi=shards[i][1]
+        li = Decimal(1)
+        xi = shards[i][0]
+        yi = shards[i][1]
         for j in range(k):
-            xj=shards[j][0]
-            if i==j:
+            xj = shards[j][0]
+            if i == j:
                 continue
-            li*= -Decimal(Decimal(xj)/(xi-xj))
-        secret+=Decimal(yi*li)
+            li *= -Decimal(Decimal(xj)/(xi-xj))
+        secret += Decimal(yi*li)
     return round(Decimal(secret))
 
 
-def getSecret(shards,k):
-    a=[Decimal(x[0]) for i,x in enumerate(shards) if i<=k]
-    b=[Decimal(y[1]) for i,y in enumerate(shards) if i<=k]
-
-    return lagrange(a,b)
-    return Polynomial(lagrange(a,b)).coef
-    # return np.linalg.solve(a,b)
-
 if __name__ == "__main__":
-    k=5
-    n=1000
-    s=3471
-    draw=True
-    shards=createShards(k,n,s,draw)
-    print(findSecrett(shards,k))
-
-
-
-
-
-
-
-
-
-
+    k = 5
+    n = 5
+    s = 3471
+    draw = True
+    shards = createShards(k, n, s, draw)
+    print(findSecrett(shards, k))
 
 
 # def findSecret(shards,k):
